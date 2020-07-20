26a31a42be58b90a0ad316344b6faa3c849d0859
Lukas Zierahn <lukas@kappa-mm.de>
2020-07-20 19:56:27 +0200

    The go code is working with the exception of that one caveat in the last commit message, trying to have it install automatically now

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 from pybindgen import retval, param, Module
 
 mod = Module('GoChest')
+
+# mod.add_container("GoSlice", "float", "set")
+
 mod.add_include('"libGoChest.h"')
-mod.add_function('Sum', retval('int'), [param('int', 'a'), param('int', 'b')])
-# mod.add_function('test', None, [])
+mod.add_function('FindChangePoints', retval('int'), [param('GoSlice', 'sequence'), param('float', 'minimumDistance')])
 mod.generate(open("GoChest.c", "w"))
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-go build -buildmode=c-shared -o libGoChest.so GoChest.go
+go build -buildmode=c-shared -o libGoChest.so GoChest.go Changepoint.go
 python3 build.py
 echo "Step 2"
 python3 setup.py install --force
