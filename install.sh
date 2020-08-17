04d0ab7e7d7246cdf235b632e0db667f1af3b32d
LukasZierahn <lukas@kappa-mm.de>
2020-08-17 15:07:43 +0100

    removed old file for debug installation

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-go build -buildmode=c-shared -o GoChest.so CWrapper.go
-python3 setup.py build_ext install
-cp /Users/lukas/Documents/Projects/GoChest/GoChest.so /Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages/GoChest-0.51-py3.8-macosx-10.9-x86_64.egg
