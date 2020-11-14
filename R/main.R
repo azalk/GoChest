6afbd142bc37c47bcf6fe9ec132f031af45ada8d
LukasZierahn <lukas@kappa-mm.de>
2020-11-14 16:22:30 +0000

    WIP: R Package

--- R/main.R
+++ R/main.R
@@ -1,10 +1,9 @@
-library(reticulate)
 py_install("PyChest", pip=TRUE)
 
 find_changepoints <- function(sequence, minimum_distance, process_count) {
-  import("PyChest")$find_changepoints(sequence, minimum_distance, process_count)
+  reticulate::import("PyChest")$find_changepoints(sequence, minimum_distance, process_count)
 }
 
 list_estimator <- function(sequence, minimum_distance) {
-  import("PyChest")$list_estimator(sequence, minimum_distance)
+  reticulate::import("PyChest")$list_estimator(sequence, minimum_distance)
 }
