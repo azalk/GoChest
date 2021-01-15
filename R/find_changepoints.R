7eccac8a70fa98abd6d8aff12c26010ba9dd68a1
Azalea K <azalea@Azaleas-iMac.local>
2021-01-15 11:27:50 +0000

    updating the comments for R functions

--- R/find_changepoints.R
+++ R/find_changepoints.R
@@ -2,13 +2,13 @@
 #'
 #' Returns the position of changepoints in the sequence
 #'
-#' @param sequence A vector of floats
+#' @param sample A vector of floats corresponding to the piecewise stationary sample where the retrospective changes are to be sought
 #' @param minimum_distance A real number between 0 and 1 corresponding to a lower-bound on the minimum normalized length of the stationary segments (as percentage of total sample length)
 #' @param process_count The different number of distinct stationary processes present. 
 #'
 #' @return The list of changepoints in increasing size
 #'
 #' @export
-find_changepoints <- function(sequence, minimum_distance, process_count) {
-  PyChest$find_changepoints(sequence, minimum_distance, process_count)
+find_changepoints <- function(sample, minimum_distance, process_count) {
+  PyChest$find_changepoints(sample, minimum_distance, process_count)
 }
--- R/list_estimator.R
+++ R/list_estimator.R
@@ -2,12 +2,12 @@
 #'
 #' Returns the position of changepoints in the sequence
 #'
-#' @param sequence A vector of floats
+#' @param sample A vector of floats corresponding to the piecewise stationary sample where the retrospective changes are to be sought
 #' @param minimum_distance A real number between 0 and 1 corresponding to a lower-bound on the minimum normalized length of the stationary segments (as percentage of total sample lengt)
 #'
 #' @return The list of changepoints in order of likelihood
 #'
 #' @export
-list_estimator <- function(sequence, minimum_distance) {
-  PyChest$list_estimator(sequence, minimum_distance)
+list_estimator <- function(sample, minimum_distance) {
+  PyChest$list_estimator(sample, minimum_distance)
 }
