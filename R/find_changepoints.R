78df9ccf0b9423799317ac49117df09ff501651c
Azalea K <azalea@azaleas-imac.home>
2021-02-11 11:52:28 +0000

    R Package

--- R/find_changepoints.R
+++ R/find_changepoints.R
@@ -1,6 +1,6 @@
 #' find_changepoints
 #'
-#' Returns the position of changepoints in the sequence
+#' Returns the position of changepoints in the sequence. NOTE: PyChest needs to be installed first by calling `install_PyChest'.
 #'
 #' @param sample A vector of floats corresponding to the piecewise stationary sample where the retrospective changes are to be sought
 #' @param minimum_distance A real number between 0 and 1 corresponding to a lower-bound on the minimum normalized length of the stationary segments (as percentage of total sample length)
@@ -8,7 +8,14 @@
 #'
 #' @return The list of changepoints in increasing size
 #'
+#' @references
+#' \insertRef{khaleghi2014asymptotically}{RChest}
+#' 
+#' \insertRef{khaleghi2012locating}{RChest}
 #' @export
 find_changepoints <- function(sample, minimum_distance, process_count) {
   PyChest$find_changepoints(sample, minimum_distance, process_count)
 }
+
+
+
--- R/list_estimator.R
+++ R/list_estimator.R
@@ -1,12 +1,13 @@
 #' list_estimator
 #'
-#' Returns the position of changepoints in the sequence
+#' Returns the position of changepoints in the sequence. NOTE: PyChest needs to be installed first by calling `install_PyChest'.
 #'
 #' @param sample A vector of floats corresponding to the piecewise stationary sample where the retrospective changes are to be sought
 #' @param minimum_distance A real number between 0 and 1 corresponding to a lower-bound on the minimum normalized length of the stationary segments (as percentage of total sample length)
 #'
 #' @return The list of changepoints in order of score
-#'
+#' @references
+#' \insertRef{khaleghi2012locating}{RChest}
 #' @export
 list_estimator <- function(sample, minimum_distance) {
   PyChest$list_estimator(sample, minimum_distance)
